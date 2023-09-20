27e36cece87f6a3e13f4447fbb776ba1b8bda8d4
Mozibix <opuzmoses@gmail.com>
2023-09-20 04:31:42 +0100

    chores: updated _error.js page

---
+++ pages/_error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div className="text-center">
+      <h1>Error Page</h1>
+      <p>Something went wrong kindly reload the page.</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
