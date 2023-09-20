14413f8c8929044035350e0334ddfef8d361baf5
Mozibix <opuzmoses@gmail.com>
2023-09-20 10:13:11 +0100

    chores: updated _error.js page component

--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,24 @@
+"use client";
+
+import { useRouter } from "next/router";
 import React from "react";
 
 function ErrorPage() {
+  const router = useRouter();
+
   return (
     <div className="text-center">
       <h1>Error Page</h1>
-      <p>Something went wrong kindly reload the page.</p>
+      <p>
+        Something went wrong kindly reload the page or{" "}
+        <span
+          className="underline cursor-pointer"
+          onClick={() => router.back()}
+        >
+          go back
+        </span>{" "}
+        .
+      </p>
     </div>
   );
 }
